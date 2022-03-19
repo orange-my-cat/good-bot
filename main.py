9b1cedb51a06f4a5f22743d6183b1602a1a52aa4
Ivan <ivan_peh@hotmail.com>
2022-03-20 02:18:41 +0800

    Added on message

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 import os
 from discord.ext import commands
 import random
+from audio_dict import audio
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -10,22 +11,25 @@ TOKEN = os.getenv('TOKEN')
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(intents=intents, command_prefix = '?')
-client.remove_command('help')
+activity = discord.Activity(type=discord.ActivityType.watching, name='?help')
+bot = commands.Bot(intents=intents, command_prefix = '?', help_command=None, activity=activity)
 
-idle = None
-
-@client.event
+@bot.event
 async def on_ready():
-  print('I am connected as {0.user}'.format(client))
-    
-@client.command()
-async def ping(ctx):
-  await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+  print('I am connected as {0.user}'.format(bot))
 
-@client.command()
+@bot.event
+async def on_message(message):
+  if message.content == "who is good":
+    await message.channel.send(random.choice(message.channel.members))
+    
+@bot.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
 async def help(ctx):
   help_text = 'This is help text.'
   await ctx.send(help_text)
     
-client.run(TOKEN)
+bot.run(TOKEN)
